4e7017ed35882bc3cbf3a795d7706c01a6b62595
Jacob Huff <jacobhuff1313@gmail.com>
2020-11-19 14:55:14 -0500

    lessons 7 and 8

--- app.js
+++ app.js
@@ -1,24 +1,56 @@
 const express = require('express');
+const morgan = require('morgan');
 
 // express app
 const app = express();
 
+// middleware
+// app.use((req, res, next) => {
+//   console.log('new request made:');
+//   console.log('host: ', req.hostname);
+//   console.log('path: ', req.path);
+//   console.log('method: ', req.method);
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   console.log('in the next middleware');
+//   next();
+// });
+
+// middleware & static files
+app.use(express.static('public'));
+
 // listen for requests
 app.listen(3000);
+app.use(morgan('dev'));
 
+// register view engine
+app.set('view engine', 'ejs');
+
+const blogs = [
+  { title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+  { title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+  { title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+];
+
+
+// index
 app.get('/', (req, res) => {
-  res.sendFile('./views/index.html', { root: _dirname });
+  res.render('index', { title: 'Home', blogs });
 });
 
+// about
 app.get('/about', (req, res) => {
-  res.sendFile('./views/about.html', { root: _dirname });
+  res.render('about', { title: 'About' });
 });
 
-app.get('/about-us', (req, res) => {
-  res.redirect('/about');
+// create
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create a new blog' });
 });
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).sendFile('./views/404.html', { root: __dirname });
+  res.status(404).render('404', { title: '404' });
 });
