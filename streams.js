d779296e318ac17dc1af2bb4aeb4a302122a03fc
Jacob Huff <jacobhuff1313@gmail.com>
2020-11-19 09:32:14 -0500

    added routing

--- streams.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-const readStream = fs.createReadStream('./docs/blog1.txt', { encoding: 'utf8' });
-const writeStream = fs.createWriteStream('./docs/blog4.txt');
-
-// piping
-readStream.pipe(writeStream);
