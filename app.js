a83583635570ceef153854f1486353c0ffd94040
Jacob Huff <jacobhuff1313@gmail.com>
2020-11-22 22:16:56 -0500

    through lesson 11

--- app.js
+++ app.js
@@ -1,43 +1,32 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const blogRoutes = require('./routes/blogRoutes');
 
 // express app
 const app = express();
 
-// middleware
-// app.use((req, res, next) => {
-//   console.log('new request made:');
-//   console.log('host: ', req.hostname);
-//   console.log('path: ', req.path);
-//   console.log('method: ', req.method);
-//   next();
-// });
-
-// app.use((req, res, next) => {
-//   console.log('in the next middleware');
-//   next();
-// });
+// connect to mongoDB
+const dbURI = 'mongodb+srv://jacobhuff:nodeblog@node.89jme.mongodb.net/node?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err))
 
 // middleware & static files
 app.use(express.static('public'));
-
-// listen for requests
-app.listen(3000);
 app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
+});
 
 // register view engine
 app.set('view engine', 'ejs');
 
-const blogs = [
-  { title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur' },
-  { title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur' },
-  { title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur' },
-];
-
-
 // index
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Home', blogs });
+  res.redirect('/blogs');
 });
 
 // about
@@ -45,10 +34,8 @@ app.get('/about', (req, res) => {
   res.render('about', { title: 'About' });
 });
 
-// create
-app.get('/blogs/create', (req, res) => {
-  res.render('create', { title: 'Create a new blog' });
-});
+// blog routes
+app.use('/blogs', blogRoutes)
 
 // 404 page
 app.use((req, res) => {
